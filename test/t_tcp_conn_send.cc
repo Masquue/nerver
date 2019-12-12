3b48da808aeab95acb2de06c8528ae45e5bdfdbc
masquue <masquue14@gmail.com>
2019-12-12 11:08:22 +0800

    Add test for EPIPE, peer closing connection

--- test/t_tcp_conn_send.cc
+++ test/t_tcp_conn_send.cc
@@ -2,6 +2,8 @@
 
 #include <cstdlib>
 
+#include <unistd.h>
+
 #include "tcp_server.h"
 
 using namespace nerver;
@@ -36,13 +38,29 @@ void on_message(tcp_conn &conn, char const *data, std::size_t len)
     conn.send(msg_send.data(), msg_send.size());
 }
 
+void on_message_sleep(tcp_conn &conn, char const *data, std::size_t len)
+{
+    if (len == 0) {
+        conn.shutdown();
+        return;
+    }
+
+    std::string msg(data, len), msg_send(msg);
+    for (unsigned i = 0; i < repetition_time; ++i)
+        msg_send += msg + std::to_string(i);
+
+    sleep(5);
+    
+    conn.send(msg_send.data(), msg_send.size());
+}
+
 int main(int argc, char **argv)
 {
     if (argc > 1)
         repetition_time = std::atoi(argv[1]);
 
     tcp_server s("9999", 6);
-    s.set_message_callback(on_message);
+    s.set_message_callback(on_message_sleep);
     s.set_establish_callback(on_connection);
     s.set_close_callback(on_close);
 
